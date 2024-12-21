24c76ac08a4401ebce4a83c1e7462bcb3d813e96
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-12-21 14:11:47 +0900

    Update Oresmith.java

--- src/main/java/com/sakalti/oresmith/Oresmith.java
+++ src/main/java/com/sakalti/oresmith/Oresmith.java
@@ -1,6 +1,15 @@
 package com.sakalti.oresmith;
 
 import net.fabricmc.api.ModInitializer;
+import com.sakalti.oresmith.items.SapphireTools;
+import com.sakalti.oresmith.items.InvarTools;
+import com.sakalti.oresmith.items.SapphireItem;
+import com.sakalti.oresmith.items.InvarItem;
+import com.sakalti.oresmith.items.NickelItem;
+import com.sakalti.oresmith.items.SapphireArmor;
+import com.sakalti.oresmith.items.InvarArmor;
+import com.sakalti.oresmith.worldgen.NickelOreFeature;
+
 
 public class Oresmith implements ModInitializer {
     public static final String MOD_ID = "oresmith";
